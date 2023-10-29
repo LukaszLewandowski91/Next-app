43f584022923cd43a2e1f50e97129d966592848b
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-10-29 18:47:44 +0100

    off sleep

--- src/ui/organisms/SuggestedProducts.tsx
+++ src/ui/organisms/SuggestedProducts.tsx
@@ -1,10 +1,10 @@
 import { ProductList } from "./ProductList";
 import { getProductsList } from "@/api/products";
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+// const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const SuggestedProductsList = async () => {
   const products = await getProductsList();
-  await sleep(5000);
+  // await sleep(5000);
   return <ProductList products={products.slice(-4)} />;
 };
